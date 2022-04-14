a438102a0e65662700bb584b54c12c129962ff2c
kruprog <70612662+kruprog@users.noreply.github.com>
2022-04-14 18:32:32 +0300

    пробую интеграционный тест с секретом

---
+++ integrationtests/russian/RussianDeclensionIntegrationTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+require_once __DIR__."/../../vendor/autoload.php";
+require_once __DIR__."/../../src/Morpher.php";
+require_once __DIR__."/../../src/WebClient.php";
+require_once __DIR__."/../../src/russian/Gender.php";
+//require_once __DIR__."/../MorpherTestHelper.php";
+require_once __DIR__."/../../src/exceptions/MorpherError.php";
+require_once __DIR__."/../../src/exceptions/IpBlocked.php";
+require_once __DIR__."/../../src/russian/exceptions/InvalidFlags.php";
+require_once __DIR__."/../../src/russian/exceptions/DeclensionNotSupportedUseSpell.php";
+require_once __DIR__."/../../src/exceptions/InvalidArgumentEmptyString.php";
+require_once __DIR__."/../../src/exceptions/InvalidServerResponse.php";
+require_once __DIR__."/../../src/russian/exceptions/RussianWordsNotFound.php";
+
+use PHPUnit\Framework\TestCase;
+
+use Morpher\Ws3Client\WebClient;
+
+
+//use Morpher\Ws3Client\Morpher;
+use Morpher\Ws3Client\Russian as Russian;
+
+
+
+final class RussianDeclensionIntegrationTest extends TestCase
+{
+    public function testSimple(): void
+    {
+        //$token='';
+        foreach (getenv() as $settingKey => $settingValue) {
+            /*if (strpos($settingKey, 'MORPHER_') === 0) {
+                ...
+            }*/
+            print ($settingKey.' = '.$settingValue."\r\n");
+            
+
+        }        
+        print("haha");
+        $this->assertTrue(true);    
+    }
+
+}
