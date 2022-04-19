e9987f23239d3f18f7077b0261ab5a6a4b193a7d
kruprog <70612662+kruprog@users.noreply.github.com>
2022-04-19 20:03:05 +0300

    ukr flags added

---
+++ src/ukrainian/Flags.php
@@ -0,0 +1,11 @@
+<?php
+namespace Morpher\Ws3Client\Ukrainian;
+
+class Flags
+{
+    const Feminine='Feminine';    //	Женский род
+    const Masculine='Masculine';  //	Мужской род
+    const Neuter='Neuter';  //Средний род
+    const Plural='Plural';  //Множественное число
+
+}
