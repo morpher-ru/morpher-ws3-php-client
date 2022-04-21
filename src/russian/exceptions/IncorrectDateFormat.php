5125dde3246387cddf570ced0c0d2c0ddcae6074
Sergey Slepov <slepov@gmail.com>
2022-04-21 22:27:23 +0100

    "Ожидается YYYY-MM-DD."

--- src/russian/exceptions/IncorrectDateFormat.php
+++ src/russian/exceptions/IncorrectDateFormat.php
@@ -3,7 +3,7 @@ namespace Morpher\Ws3Client\Russian;
 
 class IncorrectDateFormat extends \InvalidArgumentException
 {
-    function __construct(string $message='Дата указана в некорректном формате.',int $code=0)
+    function __construct(string $message='Дата указана в некорректном формате. Ожидается YYYY-MM-DD.',int $code=0)
     {
         parent::__construct($message,$code);
     }
